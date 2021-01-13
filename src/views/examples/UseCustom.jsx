e9351f35dc22f042dbe040400f0a422d61c00c8e
Simone Lopes da Silva <SIMLSILVA@firjan.com.br>
2021-01-13 14:38:10 -0300

    useCustom with fecth

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
+import { useFetch } from '../../hooks/useFetch'
 import { useCounter } from '../../hooks/userCounter'
 
 const UseRef = (props) => {
 
     const [count, inc, dec] = useCounter()
 
+    const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+    const response = useFetch(url)
+
+
+    function showStates(states){
+        return states.map(state => <li kye={state.nome}>{state.nome} - {state.sigla}</li>)
+    }
     return (
         <div className="UseCustom">
             <PageTitle
@@ -18,6 +26,12 @@ const UseRef = (props) => {
                 <span className="text">{count}</span>
                 <button className="btn" onClick={() => inc()}>+</button>
                 <button className="btn" onClick={() => dec()}>-</button>
+            </div>
+            <SectionTitle title="Exercicio #02"/>
+            <div className="center">
+                <ul>
+                    {response.data ? showStates(response.data) : false }
+                </ul>
             </div>
         </div>
     )
